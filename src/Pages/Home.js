d90a92c30eb703594600d40789578d5823baafc5
Ndungu-Muigai <ndungu.muigai01@gmail.com>
2024-07-08 22:02:51 +0300

    Redoing the character cards

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,21 +1,8 @@
-import CharacterCard from '../Components/CharacterCard'; 
-
-const Home = () => {
-    // Example data for characters
-    const characters = [
-        { id: 1, name: 'Character 1', next: 'Character 2', previous: '' },
-        { id: 2, name: 'Character 2', next: 'Character 3', previous: 'Character 1' },
-        { id: 3, name: 'Character 3', next: 'Character 4', previous: 'Character 2' },
-        // Add more characters as needed
-    ];
-
+const Home = () => 
+{
     return (
         <div className="landing h-screen bg-cover bg-center flex justify-center items-center">
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-                {characters.map((character, index) => (
-                    <CharacterCard key={character.id} character={character} />
-                ))}
-            </div>
+            
         </div>
     );
 }
